ba487f4a3e44f3df55c191e5e65213d2922bcee2
clarke021 <donobanclarke22@gmail.com>
2026-03-19 18:56:49 -0600

    fix: limpiar App.tsx

--- examen-01/App.tsx
+++ examen-01/App.tsx
@@ -1,18 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import ProductFormScreen from './screens/ProductFormScreen';
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <ProductFormScreen/>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
 import CardProducto from './components/Card';
 
 export default function App() {
   return (
-    <CardProducto nombre='leche' cantidad={10} categoria='lacteos' precio={200} />
+    <View style={styles.container}>
+      <ProductFormScreen />
+      <CardProducto nombre='leche' cantidad={10} categoria='lacteos' precio={200} />
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
@@ -20,7 +17,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
